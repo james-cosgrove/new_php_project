1863de787345de03d2e0d934f2633586f245bb1e
James Cosgrove <cosgrove.jamesc@gmail.com>
2017-05-09 11:07:00 +1000

    create connect.php to establish database connection

--- index.php
+++ index.php
@@ -2,11 +2,12 @@
 <html>
   <head>
     <meta charset="utf-8">
-    <title></title>
+    <title>Simple To-Do List</title>
+    <link rel="stylesheet" href="style.css">
   </head>
   <body>
     <div class="wrap">
-      <h1>Hello World!</h1>
+
     </div>
 
   </body>
