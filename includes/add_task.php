af1a4b0726511442b668c8530401d8962c7f53af
James Cosgrove <cosgrove.jamesc@gmail.com>
2017-05-10 14:17:02 +1000

    fixed date format in $time variable which was causing mysqli_fetch_assoc to return null

--- includes/add_task.php
+++ includes/add_task.php
@@ -1,20 +1,21 @@
 <?php
   $task = strip_tags( $_POST['task'] );
   $date = date('Y-m-d');
-  $time = date('H-i-s');
+  $time = date('H:i:s');
 
   require("connect.php");
 
   mysqli_query($conn, "INSERT INTO tasks VALUES ('', '$task', '$date', '$time')");
 
-  $query = mysqli_query($conn, "SELECT * FROM tasks WHERE task='$task' and date='$date' and time='$time'");
-
-  while( $row = mysqli_fetch_assoc($query) ) {
-    $task_id = $row['id'];
-    $task_name = $row['task'];
-  }
+  $result = mysqli_query($conn, "SELECT * FROM tasks WHERE task='$task' and date='$date' and time='$time'");
+  $row = mysqli_fetch_assoc($result);
+  $task_id = $row['id'];
+  $task_name = $row['task'];
 
   mysqli_close($conn);
 
-  echo '<li><span>'.$task_name.'</span><img id="'.$task_id.'" class="delete-button" width="10px" src="images/close.svg" /></li>';
+  echo '<li>
+          <span>'.$task_name.'</span>
+          <img id="'.$task_id.'" class="delete-button" width="10px" src="images/close.svg" />
+        </li>';
  ?>
