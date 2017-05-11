b0a84fc58255f108177d4c622a6566ab2f5debdb
James Cosgrove <cosgrove.jamesc@gmail.com>
2017-05-11 10:44:42 +1000

    change delete event handler to listen on dynamically loaded tasks

--- js/task.js
+++ js/task.js
@@ -1,28 +1,21 @@
 
-addTask();
-deleteTask();
+$('#add-new-task').submit(function() {
+  var newTask = $('#add-new-task input[name=new-task]').val();
+  if (newTask != '') {
+    $.post('includes/add_task.php', { task: newTask }, function( data ) {
+      $('#add-new-task input[name=new-task]').val('');
+      $(data).appendTo('#task-list ul').hide().fadeIn();
+    });
+  }
+  return false; // Ensure form does not submit twice
+});
 
-function addTask() {
-  $('.add-new-task').submit(function() {
-    var newTask = $('.add-new-task input[name=new-task]').val();
-    if (newTask != '') {
-      $.post('includes/add_task.php', { task: newTask}, function( data ) {
-        $('.add-new-task input[name=new-task]').val('');
-        $(data).appendTo('.task-list ul').hide().fadeIn();
-      });
-    }
-    return false; // Ensure form does not submit twice
-  });
-}
-
-function deleteTask() {
-  $('.delete-button').click(function() {
-    var currentElement = $(this);
-    var id = $(this).attr('id');
-    $.post('includes/delete_task.php', { task_id: id }, function() {
-      currentElement.parent().fadeOut("fast", function() {
-        $(this).remove();
-      });
+$('body').on('click', '.delete-button', function() {
+  var currentElement = $(this);
+  var id = $(this).attr('id');
+  $.post('includes/delete_task.php', { task_id: id }, function() {
+    currentElement.parent().fadeOut("fast", function() {
+      $(this).remove();
     });
   });
-}
+});
